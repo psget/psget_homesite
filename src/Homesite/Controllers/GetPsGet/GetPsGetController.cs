42120b7a8be61dac1a8a6eb4fe3931581053cbb4
Mike Chaliy <mike@chaliy.name>
2012-03-20 01:41:49 +0200

    Added GoogleAnalitics to GetPsGet.ps1

--- src/Homesite/Controllers/GetPsGet/GetPsGetController.cs
+++ src/Homesite/Controllers/GetPsGet/GetPsGetController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Net;
 using System.Web.Mvc;
+using Homesite.Fx;
+
 namespace Homesite.Controllers.GetPsGet
 {
     public class GetPsGetController : Controller
@@ -8,6 +10,7 @@ namespace Homesite.Controllers.GetPsGet
         {
             var client = new WebClient();
             var result = client.DownloadString("https://github.com/chaliy/psget/raw/master/GetPsGet.ps1");
+            GoogleAnalytics.SendEvent("/GetPsGet.ps1");
             return Content(result, "text/plain");
         }
     }
---
+++ src/Homesite/Fx/GoogleAnalitics.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * Bit modified code from https://gist.github.com/1972785 by Gregory Young
+ */
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Homesite.Fx
+{
+    public class GoogleAnalytics
+    {
+        // Tracker version.
+        private const string Version = "4.4sa";
+
+        // Get a random number string.
+        private static String GetRandomNumber()
+        {
+            var randomClass = new Random();
+            return randomClass.Next(0x7fffffff).ToString();
+        }
+
+        // Make a tracking request to Google Analytics from this server.
+        // Copies the headers from the original request to the new one.
+        // If request containg utmdebug parameter, exceptions encountered
+        // communicating with Google Analytics are thown.
+        private static void SendRequestToGoogleAnalytics(string utmUrl)
+        {
+            try
+            {
+                var connection = WebRequest.Create(utmUrl);
+
+                ((HttpWebRequest)connection).UserAgent = "";
+                connection.Headers.Add("Accept-Language",
+                                       "EN-US");
+
+                using (connection.GetResponse())
+                {
+                    // Ignore response
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError("Error contacting Google Analytics\r\n" + ex);
+            }
+        }
+
+        // Track a page view, updates all the cookies and campaign tracker,
+        // makes a server side request to Google Analytics and writes the transparent
+        // gif byte data to the response.
+        private static void TrackPageView(string path)
+        {
+            var utmGifLocation = "http://www.google-analytics.com/__utm.gif";
+            var visitor = Guid.NewGuid().GetHashCode();
+
+            // Construct the gif hit url.
+            var utmUrl = utmGifLocation + "?" +
+                            "utmwv=" + Version +
+                            "&utmn=" + GetRandomNumber() +
+                            "&utmhn=" + "psget.net" +
+                            "&utmr=" + "-" +
+                            "&utmp=" + path.Replace("/", "%2F") +
+                            "&utmac=" + "UA-28961030-1" +
+                            //"&utmcc=__utma%3D999.999.999.999.999.1%3B" +
+                            "&utmcc=__utma%3D999.999.999." + visitor + "." + DateTime.Now.GetHashCode() + ".1%3B" +
+                            "&utmvid=" + (visitor - DateTime.Now.GetHashCode());
+
+            SendRequestToGoogleAnalytics(utmUrl);
+        }        
+        
+        public static void SendEvent(string path)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                TrackPageView(path);
+            });
+        }
+    }
+}
