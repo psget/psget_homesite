b97d951431a78c231bac3a6a8b6f9ea8488c22f2
Mike Chaliy <mike@chaliy.name>
2012-03-26 16:34:56 +0300

    Issue GetPsGet.ps1 404 fixed

--- src/Homesite/Controllers/GetPsGet/GetPsGetController.cs
+++ src/Homesite/Controllers/GetPsGet/GetPsGetController.cs
@@ -9,7 +9,7 @@ namespace Homesite.Controllers.GetPsGet
         public ActionResult Index() 
         {
             var client = new WebClient();
-            var result = client.DownloadString("https://github.com/chaliy/psget/raw/master/GetPsGet.ps1");
+            var result = client.DownloadString("https://github.com/psget/psget/raw/master/GetPsGet.ps1");
             GoogleAnalytics.SendEvent("/GetPsGet.ps1");
             return Content(result, "text/plain");
         }
